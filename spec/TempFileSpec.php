0525220a3e7722390bc94b5eb3064dec817e0f7f
Sergey S <5818959@gmail.com>
2017-03-20 17:11:36 +0300

    [+] Adds temp file class.

---
+++ spec/TempFileSpec.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace spec\TempFileService;
+
+use TempFileService\TempFile;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class TempFileSpec extends ObjectBehavior
+{
+    function it_can_create_tmp_file_in_system_dir()
+    {
+        $this->beConstructedWith('');
+
+        $this->getDir()->shouldBe(realpath(sys_get_temp_dir()));
+    }
+
+    function it_can_create_tmp_file_in_specified_directory()
+    {
+        $this->beConstructedWith(__DIR__);
+
+        $this->getDir()->shouldBe(realpath(__DIR__));
+    }
+
+    function it_should_return_dir()
+    {
+        $this->getDir()->shouldBeString();
+    }
+
+    function it_should_return_name()
+    {
+        $this->getName()->shouldBeString();
+    }
+
+    function it_should_return_valid_path()
+    {
+        $this->getPath()->shouldBeString();
+    }
+
+    function it_can_write_content_to_file()
+    {
+        $sampleContent = 'some text string';
+
+        $this->write($sampleContent)->shouldReturn(true);
+        $this->read()->shouldReturn($sampleContent);
+    }
+
+    function it_can_read_content_from_file()
+    {
+        // running right after temp file was created
+        $this->read()->shouldReturn('');
+    }
+}
---
+++ src/TempFile.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace TempFileService;
+
+/**
+ * Temp file.
+ */
+class TempFile
+{
+    /**
+     * File dir.
+     *
+     * @var string
+     */
+    private $dir;
+    /**
+     * File name.
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * Constructor.
+     *
+     * @param string $fileDir Temp file directory
+     */
+    public function __construct($fileDir = '')
+    {
+        if (empty($fileDir)) {
+            $fileDir = sys_get_temp_dir();
+        }
+
+        $file = $this->createFile($fileDir);
+
+        $this->dir = dirname($file);
+        $this->name = basename($file);
+    }
+
+    /**
+     * Return file name.
+     *
+     * @return string File name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Return dir name.
+     *
+     * @return string Dir name
+     */
+    public function getDir()
+    {
+        return $this->dir;
+    }
+
+    /**
+     * Return the full path to file, including its name.
+     *
+     * @return string Full path to file, including its name.
+     */
+    public function getPath()
+    {
+        return $this->dir . '/' . $this->name;
+    }
+
+    /**
+     * Write data to file.
+     *
+     * @param string $data The data to write
+     *
+     * @return boolean Returns TRUE on success or FALSE on failure
+     *
+     * @throws \RuntimeException Can't create a temp file
+     */
+    public function write($data)
+    {
+        if (file_put_contents($this->getPath(), $data) !== false) {
+            return true;
+        }
+
+        throw new \RuntimeException('Can\'t write to a temp file.');
+    }
+
+    /**
+     * Read data from file.
+     *
+     * @return string File content
+     */
+    public function read()
+    {
+        return file_get_contents($this->getPath());
+    }
+
+    /**
+     * Destructor.
+     *
+     * @throws \RuntimeException Can't destroy a temp file
+     */
+    public function __destruct()
+    {
+        $this->destroyFile();
+    }
+
+    /**
+     * Create file.
+     *
+     * @param string $dir File directory
+     *
+     * @return string Full path to file, including its name
+     *
+     * @throws \RuntimeException Can't create a temp file
+     */
+    private function createFile($dir)
+    {
+        $file = tempnam($dir, '');
+
+        if (!file_exists($file)) {
+            throw new \RuntimeException('Can\'t create a temp file.');
+        }
+
+        return $file;
+    }
+
+    /**
+     * Destroy file.
+     *
+     * @return boolean Returns TRUE on success or FALSE on failure
+     *
+     * @throws \RuntimeException Can't destroy a temp file
+     */
+    private function destroyFile()
+    {
+        if (unlink($this->getPath()) === true) {
+            return true;
+        }
+
+        throw new \RuntimeException('Can\'t destroy a temp file.');
+    }
+}
