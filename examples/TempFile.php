1a2861171fb868db4ef5f190fa64e920047ff1a9
Sergey S <5818959@gmail.com>
2017-03-21 11:18:30 +0300

    [*] Updates examples.

--- examples/TempFile.php
+++ examples/TempFile.php
@@ -5,11 +5,14 @@ namespace TempFileService;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // create a temp file
+
 $tempFile = new TempFile();
 
 echo 'Temp file name: ' . $tempFile->getName() . PHP_EOL;
 echo 'Temp file dir: ' . $tempFile->getDir() . PHP_EOL;
 echo 'Temp file path: ' . $tempFile->getPath() . PHP_EOL;
+
+// write some data to it
 
 $exampleData = 'some test text';
 
@@ -19,6 +22,8 @@ if ($tempFile->write($exampleData)) {
 } else {
     echo '  ERROR.' . PHP_EOL;
 }
+
+// read data
 
 echo 'Read data from temp file:' . PHP_EOL;
 if ($data = $tempFile->read()) {
