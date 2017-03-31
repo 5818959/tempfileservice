01ffa9a97e04386c588e9c035f353dfb96b9586a
Sergey S <5818959@gmail.com>
2017-03-31 14:33:48 +0300

    [*] Refact examples.

--- examples/Service.php
+++ examples/Service.php
@@ -6,15 +6,14 @@ require_once 'support.php';
 echo 'Create a temporary file:' . PHP_EOL;
 
 $tempFile = \TempFileService\Service::create();
-
 dumpTempFileStat($tempFile);
-
 unset($tempFile);
+
+echo PHP_EOL;
 
 echo 'Create a temporary file with content:' . PHP_EOL;
 
 $exampleData = 'some test text';
-
 $tempFile = \TempFileService\Service::create(array('content' => $exampleData));
 
 echo 'Read data from temp file:' . PHP_EOL;
@@ -29,20 +28,20 @@ if ($data = $tempFile->read()) {
 } else {
     echo '  ERROR.' . PHP_EOL;
 }
-echo '---' . PHP_EOL;
-
 unset($tempFile);
 
+echo PHP_EOL;
+
 $fileName = 'spec' . uniqid(mt_rand(), true);
+
 echo 'Create a temporary file with specified name (' . $fileName . '):' . PHP_EOL;
 
 $tempFile = \TempFileService\Service::create(array('name' => $fileName));
-
 echo 'Temp file name is "' . $tempFile->getName() . '" and its '
    . ($tempFile->getName() === $fileName ? '' : 'NOT ') . 'match.' . PHP_EOL;
-echo '---' . PHP_EOL;
-
 unset($tempFile);
+
+echo PHP_EOL;
 
 $prefix = 'exp_';
 $postfix = '.exp';
@@ -54,15 +53,11 @@ $tempFile = \TempFileService\Service::create(array(
     'prefix' => $prefix,
     'postfix' => $postfix,
 ));
-
 $isMatch = false;
 if (substr($tempFile->getName(), 0, strlen($prefix)) == $prefix
     && substr($tempFile->getName(), -strlen($postfix))
 ) {
     $isMatch = true;
 }
-
 echo 'Temp file name is "' . $tempFile->getName() . '" and its '
    . ($isMatch ? '' : 'NOT ') . 'match.' . PHP_EOL;
-
-echo '---' . PHP_EOL;
--- examples/TempFile.php
+++ examples/TempFile.php
@@ -6,19 +6,21 @@ require_once 'support.php';
 echo 'Create a temp file:' . PHP_EOL;
 
 $tempFile = new \TempFileService\TempFile();
-
 dumpTempFileStat($tempFile);
+
+echo PHP_EOL;
 
 echo 'Write some data to it:' . PHP_EOL;
 
 $exampleData = 'some test text';
-
 echo 'write "' . $exampleData . '"' . PHP_EOL;
 if ($tempFile->write($exampleData)) {
     echo '  done.' . PHP_EOL;
 } else {
     echo '  ERROR.' . PHP_EOL;
 }
+
+echo PHP_EOL;
 
 echo 'Read data from temp file:' . PHP_EOL;
 
@@ -32,7 +34,8 @@ if ($data = $tempFile->read()) {
 } else {
     echo '  ERROR.' . PHP_EOL;
 }
-echo '---' . PHP_EOL;
+
+echo PHP_EOL;
 
 echo 'Destroy temp file:' . PHP_EOL;
 
@@ -43,23 +46,24 @@ if (!file_exists($filePath)) {
 } else {
     echo '  ERROR.';
 }
-echo '---' . PHP_EOL;
+
+echo PHP_EOL;
 
 echo 'Create a temp file in the specified directory (use current directory):' . PHP_EOL;
 
 $tempFile = new \TempFileService\TempFile(__DIR__);
-
 dumpTempFileStat($tempFile);
-
 unset($tempFile);
+
+echo PHP_EOL;
 
 echo 'Create a temporary file in a non-existent directory:' . PHP_EOL;
 
 $tempFile = new \TempFileService\TempFile(__DIR__ . '/non_existent');
-
 dumpTempFileStat($tempFile);
-
 unset($tempFile);
+
+echo PHP_EOL;
 
 $fileName = 'example_' . uniqid(mt_rand(), true);
 
@@ -69,7 +73,5 @@ echo 'Create a temporary file with the specified name (' . $fileName . '):' . PH
 if (file_exists(__DIR__ . '/' . $fileName)) {
     unlink(__DIR__ . '/' . $fileName);
 }
-
 $tempFile = new \TempFileService\TempFile(__DIR__, 'testFileName');
-
 dumpTempFileStat($tempFile);
